533b6238c2ecd286409eb0fef70b166d90ab682c
ByteHamster <info@bytehamster.com>
2021-08-13 14:55:39 +0200

    Added command line parser

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -5,6 +5,7 @@
 #include <EliasFanoObjectStore.h>
 #include <SeparatorObjectStore.h>
 #include <ParallelCuckooObjectStore.h>
+#include <tlx/cmdline_parser.hpp>
 
 #include "RandomObjectProvider.h"
 
@@ -118,19 +119,28 @@ static void testVariableSizeObjectStores(size_t numObjects, float fillDegree, si
 
 template<class IoManager = MemoryMapIO<>>
 void testMultipleQueryHandles(size_t numObjects, int numQueryHandles, int numBatches, int numQueriesPerBatch) {
+    std::cout<<"Parallel query handles: "<<numQueryHandles<<std::endl;
     std::vector<uint64_t> keys = randomKeys(numObjects);
-    EliasFanoObjectStore<8, VerboseBenchmarkConfig<IoManager>> objectStore("key_value_store.txt");
-    objectStore.writeToFile(keys, objectProvider);
-    objectStore.reloadFromFile();
 
-    objectStore.LOG("Syncing filesystem before query");
+    EliasFanoObjectStore<8, VerboseBenchmarkConfig<IoManager>> objectStore1("/data02/hplehmann/key_value_store.txt");
+    objectStore1.writeToFile(keys, objectProvider);
+    objectStore1.reloadFromFile();
+    EliasFanoObjectStore<8, VerboseBenchmarkConfig<IoManager>> objectStore2("/data03/hplehmann/key_value_store2.txt");
+    objectStore2.writeToFile(keys, objectProvider);
+    objectStore2.reloadFromFile();
+
+    objectStore1.LOG("Syncing filesystem before query");
     system("sync");
-    objectStore.LOG("Querying");
+    objectStore1.LOG("Querying");
 
     std::vector<typename EliasFanoObjectStore<8, VerboseBenchmarkConfig<IoManager>>::QueryHandle> queryHandles;
     queryHandles.reserve(numQueryHandles);
-for (int i = 0; i < numQueryHandles; i++) {
-        queryHandles.push_back(objectStore.newQueryHandle(numQueriesPerBatch));
+    for (int i = 0; i < numQueryHandles; i++) {
+        if ((i % 2) == 0) {
+            queryHandles.push_back(objectStore1.newQueryHandle(numQueriesPerBatch));
+        } else {
+            queryHandles.push_back(objectStore2.newQueryHandle(numQueriesPerBatch));
+        }
     }
     int currentQueryHandle = 0; // Round-robin
 
@@ -150,33 +160,62 @@ for (int i = 0; i < numQueryHandles; i++) {
     long time = std::chrono::duration_cast<std::chrono::nanoseconds>(queryEnd - queryStart).count();
     std::cout<<"\rPerformance: "<< std::round(((double)totalQueries/time)*1000*1000)
             << " kQueries/s (" << time/totalQueries << " ns/query)" <<std::endl;
+    queryHandles.at(0).stats.print();
 }
 
-int main() {
+int main(int argc, char** argv) {
     constexpr int fileFlags = O_DIRECT | O_SYNC;
-    constexpr int N = 1e6;
-    constexpr float fillDegree = 0.98;
-    int numBatches = 1e6;
+    size_t N = 1e6;
+    double fillDegree = 0.98;
+    size_t numBatches = 5e5;
+    size_t numParallelBatches = 1;
+    size_t batchSize = 10;
+    bool useMmapIo = false, usePosixIo = true, usePosixAio = false, useUringIo = false, useIoSubmit = false;
+    bool useCachedIo = false;
+    bool verifyResults = false;
+    std::vector<std::string> storeFiles;
+    storeFiles.emplace_back("key_value_store.txt");
+    size_t efParameterA = 0;
+    size_t separatorBits = 0;
+    bool cuckoo = false;
 
-    testVariableSizeObjectStores<MemoryMapIO<fileFlags>>(N, fillDegree, numBatches, 1);
+    tlx::CmdlineParser cmd;
+    cmd.add_size_t('n', "num_objects", N, "Number of objects in the data store");
+    cmd.add_double('d', "fill_degree", fillDegree, "Fill degree on the external storage. Elias-Fano method always uses 1.0");
+    cmd.add_size_t('b', "num_batches", numBatches, "Number of query batches to execute");
+    cmd.add_size_t('p', "num_parallel_batches", numBatches, "Number of parallel query batches to execute");
+    cmd.add_size_t('b', "batch_size", batchSize, "Number of keys to query per batch");
+    cmd.add_bool('v', "verify", verifyResults, "Check if the result returned from the data structure matches the expected result");
+    cmd.add_stringlist('f', "store_files", storeFiles, "Files to store the external-memory data structures in");
 
-    numBatches /= 3000; // Hard disks with O_DIRECT are slow
-    testVariableSizeObjectStores<PosixIO<fileFlags>>(N, fillDegree, numBatches, 1);
-    testVariableSizeObjectStores<PosixAIO<fileFlags>>(N, fillDegree, numBatches, 1);
-    testVariableSizeObjectStores<UringIO<fileFlags>>(N, fillDegree, numBatches, 1);
-    testVariableSizeObjectStores<LinuxIoSubmit<fileFlags>>(N, fillDegree, numBatches, 1);
+    cmd.add_size_t('e', "elias_fano", efParameterA, "Run the Elias-Fano method with the given number of bins per page");
+    cmd.add_size_t('s', "separator", separatorBits, "Run the separator method with the given number of separator bits");
+    cmd.add_bool('c', "cuckoo", cuckoo, "Run the cuckoo method");
 
-    numBatches /= 10; // Multiple queries per batch
-    testVariableSizeObjectStores<PosixIO<fileFlags>>(N, fillDegree, numBatches, 10);
-    testVariableSizeObjectStores<PosixAIO<fileFlags>>(N, fillDegree, numBatches, 10);
-    testVariableSizeObjectStores<UringIO<fileFlags>>(N, fillDegree, numBatches, 10);
-    testVariableSizeObjectStores<LinuxIoSubmit<fileFlags>>(N, fillDegree, numBatches, 10);
+    cmd.add_bool('m', "mmap_io", useMmapIo, "Include Memory Map IO benchmarks");
+    cmd.add_bool('r', "posix_io", usePosixIo , "Include Posix (read()) file IO benchmarks");
+    cmd.add_bool('a', "posix_aio", usePosixAio , "Include Posix AIO benchmarks");
+    cmd.add_bool('u', "uring_io", useUringIo , "Include Linux Uring IO benchmarks");
+    cmd.add_bool('i', "io_submit", useIoSubmit , "Include Linux io_submit syscall for IO benchmarks");
+    cmd.add_bool('c', "cache_io", useCachedIo , "Allow the system to cache IO in RAM instead of using O_DIRECT");
 
-    /*int numQueriesPerBatch = 10;
-    testMultipleQueryHandles<UringIO<fileFlags>>(N, 1, numBatches, numQueriesPerBatch);
-    testMultipleQueryHandles<UringIO<fileFlags>>(N, 5, numBatches, numQueriesPerBatch);
-    testMultipleQueryHandles<UringIO<fileFlags>>(N, 10, numBatches, numQueriesPerBatch);
-    testMultipleQueryHandles<UringIO<fileFlags>>(N, 20, numBatches, numQueriesPerBatch);
-    testMultipleQueryHandles<UringIO<fileFlags>>(N, 30, numBatches, numQueriesPerBatch);*/
+    if (!cmd.process(argc, argv)) {
+        cmd.print_usage();
+        return 1;
+    } else if (storeFiles.empty()) {
+        std::cerr<<"No output files specified"<<std::endl;
+        cmd.print_usage();
+        return 1;
+    } else if (numParallelBatches % storeFiles.size() != 0) {
+        std::cerr<<"Number of parallel batches must be a multiple of the number of store files"<<std::endl;
+        return 1;
+    } else if (!cuckoo && separatorBits == 0 && efParameterA == 0) {
+        std::cerr<<"No method specified"<<std::endl;
+        cmd.print_usage();
+        return 1;
+    }
+
+    testVariableSizeObjectStores<LinuxIoSubmit<fileFlags>>(N, fillDegree, numBatches, batchSize);
+    testMultipleQueryHandles<LinuxIoSubmit<fileFlags>>(N, numParallelBatches, numBatches, batchSize);
     return 0;
 }
