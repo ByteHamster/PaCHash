ecb5dd214202d8d871b74277bba61734e6084bbc
ByteHamster <info@bytehamster.com>
2021-10-06 18:30:10 +0200

    Default to objects with 4*blocks size

--- include/PageConfig.h
+++ include/PageConfig.h
@@ -3,5 +3,5 @@
 class PageConfig {
     public:
         static constexpr size_t PAGE_SIZE = 4096;
-        static constexpr size_t MAX_OBJECT_SIZE = 4096;
+        static constexpr size_t MAX_OBJECT_SIZE = 16384;
 };
