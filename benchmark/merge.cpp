216789b6699501858ae6b4161998c6378bf98ce6
ByteHamster <info@bytehamster.com>
2021-10-13 15:28:20 +0200

    Support multiple merge iterations

--- benchmark/merge.cpp
+++ benchmark/merge.cpp
@@ -101,24 +101,7 @@ class LinearObjectReader {
         }
 };
 
-int main(int argc, char** argv) {
-    std::vector<std::string> inputFiles;
-    std::string outputFile;
-
-    tlx::CmdlineParser cmd;
-    cmd.add_stringlist('i', "input_file", inputFiles, "Input file that should be merged. Can be specified multiple times");
-    cmd.add_string('o', "output_file", outputFile, "File to write the merged data structure to");
-
-    if (!cmd.process(argc, argv)) {
-        return 1;
-    }
-
-    if (inputFiles.empty() || outputFile.empty()) {
-        std::cerr<<"Need input and output files"<<std::endl;
-        cmd.print_usage();
-        return 1;
-    }
-
+void benchmarkMerge(std::vector<std::string> &inputFiles, std::string &outputFile) {
     auto time1 = std::chrono::high_resolution_clock::now();
     std::vector<LinearObjectReader> readers;
     readers.reserve(inputFiles.size());
@@ -133,10 +116,11 @@ int main(int argc, char** argv) {
 
     LinearObjectWriter writer(outputFile.c_str(), totalBlocks);
     size_t readersCompleted = 0;
+    size_t totalObjects = 0;
 
     while (readersCompleted < readers.size()) {
         size_t minimumReader = -1;
-        uint64_t minimumKey = -1;
+        uint64_t minimumKey = ~0;
         for (size_t i = 0; i < readers.size(); i++) {
             LinearObjectReader &reader = readers[i];
             if (reader.completed) {
@@ -152,11 +136,11 @@ int main(int argc, char** argv) {
 
         LinearObjectReader &minReader = readers[minimumReader];
         writer.write(minReader.currentKey(), minReader.currentLength(), minReader.currentContent());
+        totalObjects++;
 
         if (minReader.hasMore()) {
             minReader.next();
-        }
-        if (!minReader.hasMore()) {
+        } else {
             readersCompleted++;
             minReader.completed = true;
         }
@@ -166,11 +150,42 @@ int main(int argc, char** argv) {
     auto time2 = std::chrono::high_resolution_clock::now();
     writer.close();
     VariableSizeObjectStore::LOG("Flushing");
-    system("sync");
+    int result = system("sync");
+    (void) result;
     VariableSizeObjectStore::LOG(nullptr);
     auto time3 = std::chrono::high_resolution_clock::now();
 
-    std::cout<<"RESULT merge="<<std::chrono::duration_cast<std::chrono::milliseconds>(time2 - time1).count()
-             <<" sync="<<std::chrono::duration_cast<std::chrono::milliseconds>(time3 - time2).count()<<std::endl;
+    std::cout << "Merging completed" << std::endl; // Needed to avoid \r in front of RESULT line
+    std::cout << "RESULT"
+              << " files=" << readers.size()
+              << " objects=" << totalObjects
+              << " merge=" << std::chrono::duration_cast<std::chrono::nanoseconds>(time2 - time1).count()
+              << " sync=" << std::chrono::duration_cast<std::chrono::nanoseconds>(time3 - time2).count()
+              << std::endl;
+}
+
+int main(int argc, char** argv) {
+    std::vector<std::string> inputFiles;
+    std::string outputFile;
+    size_t iterations = 1;
+
+    tlx::CmdlineParser cmd;
+    cmd.add_stringlist('i', "input_file", inputFiles, "Input file that should be merged. Can be specified multiple times");
+    cmd.add_string('o', "output_file", outputFile, "File to write the merged data structure to");
+    cmd.add_size_t('n', "iterations", iterations, "Merge multiple times");
+
+    if (!cmd.process(argc, argv)) {
+        return 1;
+    }
+
+    if (inputFiles.empty() || outputFile.empty()) {
+        std::cerr<<"Need input and output files"<<std::endl;
+        cmd.print_usage();
+        return 1;
+    }
+
+    for (int i = 0; i < iterations; i++) {
+        benchmarkMerge(inputFiles, outputFile);
+    }
     return 0;
 }
