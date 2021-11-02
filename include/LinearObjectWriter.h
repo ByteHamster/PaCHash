587d500a751d0feb88370f8a31b23d37fe162499
ByteHamster <info@bytehamster.com>
2021-11-02 17:17:42 +0100

    Useless micro optimization

--- include/LinearObjectWriter.h
+++ include/LinearObjectWriter.h
@@ -6,21 +6,20 @@ class LinearObjectWriter {
         StoreConfig::length_t offset = 0;
         int fd;
         size_t numObjectsOnPage = 0;
-        std::array<StoreConfig::key_t, StoreConfig::BLOCK_LENGTH / VariableSizeObjectStore::overheadPerObject> keys = {};
-        std::array<StoreConfig::length_t, StoreConfig::BLOCK_LENGTH / VariableSizeObjectStore::overheadPerObject> lengths = {};
+        StoreConfig::key_t keys[StoreConfig::BLOCK_LENGTH / VariableSizeObjectStore::overheadPerObject] = {0};
+        StoreConfig::length_t lengths[StoreConfig::BLOCK_LENGTH / VariableSizeObjectStore::overheadPerObject] = {0};
         StoreConfig::length_t spaceLeftOnBlock = StoreConfig::BLOCK_LENGTH - VariableSizeObjectStore::overheadPerBlock;
         size_t blockWritingPosition = 0;
         char *currentBlock = nullptr;
         char *buffer1 = nullptr;
         char *buffer2 = nullptr;
-        const char *filename = nullptr;
         size_t fileSizeBlocks = 0;
         UringIO ioManager;
     public:
         size_t blocksGenerated = 0;
 
         explicit LinearObjectWriter(const char *filename, int flags)
-                : filename(filename), ioManager(filename, O_CREAT | flags, 2) {
+                : ioManager(filename, O_CREAT | flags, 2) {
             fd = open(filename, O_RDWR | O_CREAT | flags, 0666);
             if (fd < 0) {
                 std::cerr<<"Error opening file: "<<strerror(errno)<<std::endl;
@@ -45,12 +44,9 @@ class LinearObjectWriter {
             numObjectsOnPage++;
             spaceLeftOnBlock -= VariableSizeObjectStore::overheadPerObject;
 
-            while (written < length) {
+            do {
                 StoreConfig::length_t toWrite = std::min(spaceLeftOnBlock, static_cast<StoreConfig::length_t>(length - written));
                 memcpy(currentBlock + blockWritingPosition, content + written, toWrite);
-                if (blockWritingPosition == 0 && written != 0) {
-                    offset = toWrite;
-                }
                 blockWritingPosition += toWrite;
                 spaceLeftOnBlock -= toWrite;
                 written += toWrite;
@@ -58,21 +54,21 @@ class LinearObjectWriter {
                 if (spaceLeftOnBlock <= VariableSizeObjectStore::overheadPerObject) {
                     // No more object fits on the page.
                     writeTable(false);
+                    offset = length - written;
                 }
-            }
+            } while (written < length);
         }
 
         void writeTable(bool forceFlush) {
             assert(blockWritingPosition <= StoreConfig::BLOCK_LENGTH);
             VariableSizeObjectStore::BlockStorage storage = VariableSizeObjectStore::BlockStorage::init(currentBlock, offset, numObjectsOnPage);
-            memcpy(&storage.lengths[0], lengths.data(), numObjectsOnPage * sizeof(StoreConfig::length_t));
-            memcpy(&storage.keys[0], keys.data(), numObjectsOnPage * sizeof(StoreConfig::key_t));
+            memcpy(&storage.lengths[0], &lengths[0], numObjectsOnPage * sizeof(StoreConfig::length_t));
+            memcpy(&storage.keys[0], &keys[0], numObjectsOnPage * sizeof(StoreConfig::key_t));
             numObjectsOnPage = 0;
             blocksGenerated++;
             currentBlock += StoreConfig::BLOCK_LENGTH;
             blockWritingPosition = 0;
             spaceLeftOnBlock = StoreConfig::BLOCK_LENGTH - VariableSizeObjectStore::overheadPerBlock;
-            offset = 0;
 
             if (currentBlock >= buffer1 + BLOCK_FLUSH * StoreConfig::BLOCK_LENGTH || forceFlush) {
                 // Flush
