3b0631029569159adfad7cfb29e1a0b659294e2a
ByteHamster <info@bytehamster.com>
2021-11-10 17:10:33 +0100

    Add small benchmark to Wikipedia example

--- example/wikipedia.cpp
+++ example/wikipedia.cpp
@@ -2,18 +2,22 @@
 #include <libxml/xmlreader.h>
 #include <lz4hc.h>
 #include <regex>
+#include <tlx/cmdline_parser.hpp>
 
-void construct(const char *inputFile, const char *outputFile) {
+size_t maxArticleSize = 1024 * 1024;
+std::vector<std::pair<std::string, std::string>> wikipediaPages;
+std::string inputFile = "enwiki-20210720-pages-meta-current1.xml";
+std::string storeFile = "wikipedia_store.txt";
+
+void construct() {
     std::string name;
     std::string value;
-    std::vector<std::pair<std::string, std::string>> wikipediaPages;
-    size_t compressionMaxSize = 1*1024*1024;
-    char* compressionTargetBuffer = new char[compressionMaxSize];
+    char* compressionTargetBuffer = new char[maxArticleSize];
 
     xmlTextReaderPtr reader;
-    reader = xmlReaderForFile(inputFile, nullptr, 0);
+    reader = xmlReaderForFile(inputFile.c_str(), nullptr, 0);
     if (reader == nullptr) {
-        fprintf(stderr, "Unable to open %s\n", inputFile);
+        fprintf(stderr, "Unable to open %s\n", inputFile.c_str());
         exit(1);
     }
     int ret = 1;
@@ -30,6 +34,10 @@ void construct(const char *inputFile, const char *outputFile) {
                 if (wikipediaPages.size() % 177 == 0) {
                     std::cout<<"\r\033[KRead: "<<wikipediaPages.size()<<" ("<<name<<")"<<std::flush;
                 }
+                if (wikipediaPages.size() >= 5000) {
+                    ret = 0;
+                    break;
+                }
             }
         } else if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT) {
             if (memcmp("title", tagName, 5) == 0) {
@@ -39,8 +47,9 @@ void construct(const char *inputFile, const char *outputFile) {
                 xmlTextReaderRead(reader);
                 char *pageContentUncompressed = (char*) xmlTextReaderConstValue(reader);
                 const size_t pageContentSize = strlen(pageContentUncompressed);
+                assert(pageContentSize < maxArticleSize);
                 const int compressedSize = LZ4_compress_HC(
-                        pageContentUncompressed, compressionTargetBuffer, pageContentSize, compressionMaxSize, 9);
+                        pageContentUncompressed, compressionTargetBuffer, pageContentSize, maxArticleSize, 9);
                 assert(compressedSize > 0);
                 value = std::string(compressionTargetBuffer, compressedSize);
             }
@@ -48,33 +57,26 @@ void construct(const char *inputFile, const char *outputFile) {
     }
     xmlFreeTextReader(reader);
     if (ret != 0) {
-        fprintf(stderr, "%s : failed to parse\n", inputFile);
+        fprintf(stderr, "%s : failed to parse\n", inputFile.c_str());
     }
     xmlCleanupParser();
     delete[] compressionTargetBuffer;
     std::cout<<"\r\033[KRead "<<wikipediaPages.size()<<" pages"<<std::endl;
 
-    EliasFanoObjectStore<8> eliasFanoStore(1.0, outputFile, 0);
+    EliasFanoObjectStore<8> eliasFanoStore(1.0, storeFile.c_str(), 0);
     eliasFanoStore.writeToFile(wikipediaPages);
     eliasFanoStore.reloadFromFile();
     eliasFanoStore.printConstructionStats();
 }
 
-int main() {
-    size_t maxArticleSize = 500 * 1024;
-    if (StoreConfig::MAX_OBJECT_SIZE < maxArticleSize) {
-        std::cerr<<"Wikipedia articles are long. The library needs to be compiled with more bits for object lengths."<<std::endl;
-        std::cerr<<"Also, using block sizes that are significantly smaller than the average object size is not efficient."<<std::endl;
-        exit(1);
-    }
-    construct("enwiki-20210720-pages-meta-current1.xml", "wikipedia_store.txt");
-
-    EliasFanoObjectStore<8> eliasFanoStore(1.0, "wikipedia_store.txt", 0);
+void interactive() {
+    EliasFanoObjectStore<8> eliasFanoStore(1.0, storeFile.c_str(), 0);
     eliasFanoStore.reloadFromFile();
     ObjectStoreView<EliasFanoObjectStore<8>, PosixIO> objectStoreView(eliasFanoStore, 0, 1);
     VariableSizeObjectStore::QueryHandle queryHandle;
     queryHandle.buffer = new (std::align_val_t(StoreConfig::BLOCK_LENGTH)) char[eliasFanoStore.requiredBufferPerQuery()];
 
+    std::cout<<"\r\033[K"<<std::endl<<"Interactive mode!"<<std::endl;
     char *articleDecompressed = new char[maxArticleSize];
     std::string line;
     while (true) {
@@ -101,4 +103,73 @@ int main() {
     }
 
     delete[] queryHandle.buffer;
+}
+
+void benchmark() {
+    size_t depth = 128;
+    EliasFanoObjectStore<8> eliasFanoStore(1.0, storeFile.c_str(), 0);
+    eliasFanoStore.reloadFromFile();
+    ObjectStoreView<EliasFanoObjectStore<8>, UringIO> objectStoreView(eliasFanoStore, 0, depth);
+    std::vector<VariableSizeObjectStore::QueryHandle> queryHandles(depth);
+    for (auto &handle : queryHandles) {
+        handle.buffer = new (std::align_val_t(StoreConfig::BLOCK_LENGTH)) char[eliasFanoStore.requiredBufferPerQuery()];
+    }
+
+    char *articleDecompressed = new char[maxArticleSize];
+
+    std::cout<<"Benchmarking query performance..."<<std::flush;
+    auto queryStart = std::chrono::high_resolution_clock::now();
+    size_t handled = 0;
+    for (size_t i = 0; i < depth; i++) {
+        queryHandles[i].prepare(wikipediaPages.at(rand() % wikipediaPages.size()).first);
+        objectStoreView.submitSingleQuery(&queryHandles[i]);
+        handled++;
+    }
+    objectStoreView.submit();
+    while (handled < 50000) {
+        VariableSizeObjectStore::QueryHandle *handle = objectStoreView.awaitAny();
+        do {
+            const int decompressedSize = LZ4_decompress_safe(handle->resultPtr, articleDecompressed, handle->length, maxArticleSize);
+            assert(decompressedSize >= 0);
+            handle->prepare(wikipediaPages.at(rand() % wikipediaPages.size()).first);
+            objectStoreView.submitSingleQuery(handle);
+            handle = objectStoreView.peekAny();
+            handled++;
+        } while (handle != nullptr);
+        objectStoreView.submit();
+    }
+    for (size_t i = 0; i < depth; i++) {
+        VariableSizeObjectStore::QueryHandle *handle = objectStoreView.awaitAny();
+        const int decompressedSize = LZ4_decompress_safe(handle->resultPtr, articleDecompressed, handle->length, maxArticleSize);
+        assert(decompressedSize >= 0);
+        handled++;
+    }
+
+    auto queryEnd = std::chrono::high_resolution_clock::now();
+    long timeMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(queryEnd - queryStart).count();
+    std::cout << "\rQuery benchmark: " << handled << " queries in " << timeMilliseconds << " ms, "
+              <<(double)handled/(double)timeMilliseconds << " kQueries/s" << std::endl;
+
+    for (auto &handle : queryHandles) {
+        delete[] handle.buffer;
+    }
+}
+
+int main(int argc, char** argv) {
+    if (StoreConfig::MAX_OBJECT_SIZE < maxArticleSize) {
+        std::cerr<<"Wikipedia articles are long. The library needs to be compiled with more bits for object lengths."<<std::endl;
+        std::cerr<<"Also, using block sizes that are significantly smaller than the average object size is not efficient."<<std::endl;
+        exit(1);
+    }
+    tlx::CmdlineParser cmd;
+    cmd.add_string('i', "input_file", inputFile, "Wikipedia xml input file");
+    cmd.add_string('o', "output_file", storeFile, "Object store output file");
+
+    if (!cmd.process(argc, argv)) {
+        return 1;
+    }
+
+    construct();
+    benchmark();
+    interactive();
 }
